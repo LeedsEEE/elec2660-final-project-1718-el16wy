0ce48dc4388a745623bb8578839e727189567463
WEIBIN YUAN <yuan@pc-139-128.customer.algernon.electric.lan>
2017-11-06 21:15:56 +0000

    Initial Commit

---
+++ el16wy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  el16wy
+//
+//  Created by WEIBIN YUAN on 2017/11/6.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
