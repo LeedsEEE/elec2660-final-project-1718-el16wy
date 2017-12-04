86519a905cd82ebdf07cc63e2ab1464a35cd7389
WEIBIN YUAN <yuan@pc-134-52.customer.algernon.electric.lan>
2017-12-04 17:08:13 +0000

    storyboard well planned

--- el16wy/ViewController.h
+++ el16wy/ViewController.h
@@ -11,13 +11,26 @@
 @interface ViewController : UIViewController
 - (IBAction)mySwitch:(UISwitch *)sender;
 - (IBAction)mySlider:(UISlider *)sender;
+- (IBAction)plus:(UIButton *)sender;
+- (IBAction)minus:(UIButton *)sender;
 
-
+//suggestion value
 @property (weak, nonatomic) IBOutlet UILabel *Gender;
 @property (weak, nonatomic) IBOutlet UILabel *Weight;
 @property (weak, nonatomic) IBOutlet UILabel *Suggestion;
 @property (weak, nonatomic) IBOutlet UISlider *kgValue;
 @property (weak, nonatomic) IBOutlet UISwitch *MF;
+
+//waterfill
+@property (weak, nonatomic) IBOutlet UIImageView *img1;
+@property (weak, nonatomic) IBOutlet UIImageView *img2;
+@property (weak, nonatomic) IBOutlet UIImageView *img3;
+@property (weak, nonatomic) IBOutlet UIImageView *img4;
+@property (weak, nonatomic) IBOutlet UIImageView *img5;
+@property (weak, nonatomic) IBOutlet UIImageView *img6;
+@property (weak, nonatomic) IBOutlet UIImageView *img7;
+@property (weak, nonatomic) IBOutlet UIImageView *img8;
+
 
 
 @end
--- el16wy/ViewController.m
+++ el16wy/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -17,6 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // empty glass
+    self.img1.hidden = true;
+    self.img2.hidden = true;
+    self.img3.hidden = true;
+    self.img4.hidden = true;
+    self.img5.hidden = true;
+    self.img6.hidden = true;
+    self.img7.hidden = true;
+    self.img8.hidden = true;
+    
 }
 
 
@@ -26,7 +38,7 @@
 }
 
 
-//choose gender: on for male and off for female, then it will show the suggestion drinking value
+// choose gender: on for male and off for female, then it will show the suggestion drinking value
 - (IBAction)mySwitch:(UISwitch *)sender {
     if (sender.on) {
         self.Gender.text = @"Male";
@@ -38,7 +50,7 @@
 }
 
 
-//move slider to choose weight and it will show the suggestion drinking value
+// move slider to choose weight and it will show the suggestion drinking value
 - (IBAction)mySlider:(UISlider *)sender {
     self.Weight.text = [NSString stringWithFormat:@"Weight = %.f kg", sender.value];
     if (self.MF.on) {
@@ -46,6 +58,17 @@
     } else {
         self.Suggestion.text = [NSString stringWithFormat:@"Drink %.f mL per glass", self.kgValue.value*35*13/25/8];
     }
+}
+
+
+// add water
+- (IBAction)plus:(UIButton *)sender {
+
+}
+
+// pour water
+- (IBAction)minus:(UIButton *)sender {
+
 }
 
 
